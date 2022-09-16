23931d0b78a4013e70361b0803b4546362ab021f
chosunghyuk <mnp1031@naver.com>
2022-09-16 13:51:36 +0900

    Update, index,registration add

--- public/js/script.js
+++ public/js/script.js
@@ -1,7 +1,43 @@
-// function myFunction() {
-//   var x = document.getElementById("textarea").value;
-//   document.getElementById("demo").innerHTML = x;
-// }
+function check() {
+  let ID = document.getElementById("exampleInputEmail1").value;
+  let password = document.getElementById("exampleInputPassword1").value;
+  let script = document.getElementById("script").value;
+  // alert(ID.length)
+  if (ID.length != 0 && password.length != 0 && script.length != 0) {
+    alert("등록 완료"); // 모두 잘 쳐졌을 때 post로 데이터 보내야함 임시방편으로 코드 작성
+  } else {
+    if (ID.length == 0) {
+      alert("ID를 입력해주세요!");
+    } else if (password.length == 0) {
+      alert("Password를 입력해주세요!");
+    } else if (script.length == 0) {
+      alert("내용을 입력해주세요!");
+    }
+  }
+}
 function a() {
   alert("Hello");
+}
+
+function confirm_cancle() {
+  let result = confirm("취소하시겠습니까?");
+  if (result == true) {
+    history.back();
+  } else {
+    alert("다시 작성해주세요!");
+  }
+}
+
+function update() {
+  let script = document.getElementById("script").value;
+  alert("수정 완료"); // 이전 script와 수정된 script 값이 다르면 수정 완료
+}
+
+function confirm_cancle() {
+  let result = confirm("취소하시겠습니까?");
+  if (result == true) {
+    location.href = "/";
+  } else {
+    alert("다시 작성해주세요!");
+  }
 }
--- server.js
+++ server.js
@@ -2,8 +2,10 @@ const express = require("express");
 const app = express();
 const path = require("path");
 
-app.use(express.static(__dirname + "/public"));
+app.use("/public", express.static(path.join(__dirname + "/public")));
+// app.use(express.static(__dirname + "/public"));
 // app.use(express.static("public"));
+
 app.listen(8080, function () {
   console.log("listening on 8080");
 });
@@ -14,4 +16,10 @@ app.get("/", function (req, res) {
 
 app.get("/post_registration.html", function (req, res) {
   res.sendFile(path.join(__dirname + "/post_registration.html"));
+});
+app.get("/post_update.html", function (req, res) {
+  res.sendFile(path.join(__dirname + "/post_update.html"));
+});
+app.get("/member_confirmation.html", function (req, res) {
+  res.sendFile(path.join(__dirname + "/member_confirmation.html"));
 });

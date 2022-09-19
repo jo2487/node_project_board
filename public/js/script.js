9a349c3b896e1e2acebb912101cf444df5ad7d17
chosunghyuk <mnp1031@naver.com>
2022-09-19 13:53:11 +0900

    question

--- public/js/script.js
+++ public/js/script.js
@@ -1,38 +1,45 @@
 const db = require("../../database.js");
+// db.query("SELECT * FROM board_table", (error, rows, fields) => {
+//   if (error) throw error;
+//   // console.log("User info is", rows);
+//   // console.log(rows[0].user_id);
+//   str = rows;
+//   return str;
+// });
+function a() {
+  db.query("SELECT * FROM board_table", (error, rows, fields) => {
+    if (error) throw error;
 
-function check() {
-  let ID = document.getElementById("exampleInputEmail1").value;
-  let password = document.getElementById("exampleInputPassword1").value;
-  let script = document.getElementById("script").value;
-  // alert(ID.length)
-  if (ID.length != 0 && password.length != 0 && script.length != 0) {
-    alert("등록 완료"); // 모두 잘 쳐졌을 때 post로 데이터 보내야함 임시방편으로 코드 작성
-  } else {
-    if (ID.length == 0) {
-      alert("ID를 입력해주세요!");
-    } else if (password.length == 0) {
-      alert("Password를 입력해주세요!");
-    } else if (script.length == 0) {
-      alert("내용을 입력해주세요!");
-    }
-  }
+    alert(rows[0].user_id);
+  });
 }
-function update() {
-  let script = document.getElementById("script").value;
-  alert("수정 완료"); // 이전 script와 수정된 script 값이 다르면 수정 완료
-}
+// function check() {
+//   let ID = document.getElementById("exampleInputEmail1").value;
+//   let password = document.getElementById("exampleInputPassword1").value;
+//   let script = document.getElementById("script").value;
+//   // alert(ID.length)
+//   if (ID.length != 0 && password.length != 0 && script.length != 0) {
+//     alert("등록 완료"); // 모두 잘 쳐졌을 때 post로 데이터 보내야함 임시방편으로 코드 작성
+//   } else {
+//     if (ID.length == 0) {
+//       alert("ID를 입력해주세요!");
+//     } else if (password.length == 0) {
+//       alert("Password를 입력해주세요!");
+//     } else if (script.length == 0) {
+//       alert("내용을 입력해주세요!");
+//     }
+//   }
+// }
+// function update() {
+//   let script = document.getElementById("script").value;
+//   alert("수정 완료"); // 이전 script와 수정된 script 값이 다르면 수정 완료
+// }
 
-function confirm_cancle() {
-  let result = confirm("취소하시겠습니까?");
-  if (result == true) {
-    location.href = "/";
-  } else {
-    alert("다시 작성해주세요!");
-  }
-}
-
-db.query("SELECT * FROM board_table", (error, rows, fields) => {
-  if (error) throw error;
-  console.log("User info is", rows);
-  console.log(rows[0].user_id);
-});
+// function confirm_cancle() {
+//   let result = confirm("취소하시겠습니까?");
+//   if (result == true) {
+//     location.href = "/";
+//   } else {
+//     alert("다시 작성해주세요!");
+//   }
+// }
